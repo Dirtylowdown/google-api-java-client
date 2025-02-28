1be14bc6d69c671e699fa680eade8d9eb120234a
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-28 17:41:49 -0600

    Update GoogleApache5HttpTransport.java
    Revoke

--- google-api-client-apache-v5/src/main/java/com/google/api/client/googleapis/apache/v5/GoogleApache5HttpTransport.java
+++ google-api-client-apache-v5/src/main/java/com/google/api/client/googleapis/apache/v5/GoogleApache5HttpTransport.java
@@ -1,53 +1,76 @@
-/*
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
+end
+void
+revoke
+stop
+delete
 
-package com.google.api.client.googleapis.apache.v5;
 
-import com.google.api.client.googleapis.GoogleUtils;
-import com.google.api.client.googleapis.mtls.MtlsProvider;
-import com.google.api.client.googleapis.mtls.MtlsUtils;
-import com.google.api.client.googleapis.util.Utils;
-import com.google.api.client.http.apache.v5.Apache5HttpTransport;
-import com.google.api.client.util.SslUtils;
-import com.google.common.annotations.Beta;
-import com.google.common.annotations.VisibleForTesting;
-import java.io.IOException;
-import java.net.ProxySelector;
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
-import java.util.concurrent.TimeUnit;
-import javax.net.ssl.SSLContext;
-import org.apache.hc.client5.http.config.ConnectionConfig;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.client5.http.impl.routing.SystemDefaultRoutePlanner;
-import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
-import org.apache.hc.client5.http.socket.LayeredConnectionSocketFactory;
-import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.core5.http.config.Registry;
-import org.apache.hc.core5.http.config.RegistryBuilder;
 
-/**
- * Utilities for Google APIs based on {@link Apache5HttpTransport}.
- *
- * @since 2.6.1
- */
-public final class GoogleApache5HttpTransport {
 
-  /**
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    * Returns a new instance of {@link Apache5HttpTransport} that uses {@link
    * GoogleUtils#getCertificateTrustStore()} for the trusted certificates. If
    * `GOOGLE_API_USE_CLIENT_CERTIFICATE` environment variable is set to "true", and the default
